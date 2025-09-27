4cda2a4bae99da0c6d97a3234fbc30d7600bbb4f
samoscon <53895420+samoscon@users.noreply.github.com>
2025-09-27 12:38:45 +0200

    Update autoload.php with new name of Repository

--- autoload.php
+++ autoload.php
@@ -3,8 +3,8 @@ spl_autoload_register(function ($class_name) {
     if(preg_match('/\\\\/', $class_name)) {
         $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
     }
-    if(file_exists("vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."controllerFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
-        require_once "vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."oratorium".DIRECTORY_SEPARATOR."controllerFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
+    if(file_exists("vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."controller-framework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
+        require_once "vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."controller-framework".DIRECTORY_SEPARATOR."{$class_name}.php";
     }
     if(file_exists("MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
         require_once "MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
