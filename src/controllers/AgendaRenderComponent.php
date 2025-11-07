a72773800b765031610400566d4d403187f50435
samoscon <53895420+samoscon@users.noreply.github.com>
2025-11-07 13:28:54 +0100

    Move all files to src folder

---
+++ src/controllers/AgendaRenderComponent.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * AgendaRenderComponent.php
+ *
+ * @package controllers
+ * @version 4.0
+ * @copyright (c) 2024, Dirk Van Meirvenne
+ * @author Dirk Van Meirvenne <van.meirvenne.dirk at gmail.com>
+ */
+namespace controllerframework\controllers;
+
+use controllerframework\registry\Request;
+
+/**
+ * Renders an Agenda entry to the browser
+ *
+ * @link ../graphs/controllers%20(Application%20Controller)%20Class%20Diagram.svg Controllers class diagram
+ * @author Dirk Van Meirvenne <van.meirvenne.dirk at gmail.com>
+ */
+class AgendaRenderComponent extends DatarequestRenderComponent {
+    
+    /**
+     * Renders an agenda entry to the browser
+     * 
+     * @param \registry\Request $request The $request must contain a Conf with 'filename'
+     * and a Conf with 'results'. 
+     * The filename will be suffixed with .ics
+     * 
+     * The results is in the format of an Agenda entry, i.e. 
+     * "BEGIN:VCALENDAR
+     * ....
+     * END:VCALENDAR"
+     */
+    #[\Override]
+    public function render(Request $request): void {
+        header('Content-type: text/calendar; charset=utf-8');
+        header('Content-Disposition: attachment; filename='.$request->get('filename').'.ics');
+        echo $request->get('results');        
+    }
+}
