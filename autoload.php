1b06d6a721e6d9e5fca13540100ec65a9f5e4156
samoscon <53895420+samoscon@users.noreply.github.com>
2025-11-07 13:32:21 +0100

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -3,9 +3,6 @@ spl_autoload_register(function ($class_name) {
     if(preg_match('/\\\\/', $class_name)) {
         $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
     }
-    if(file_exists("vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."controller-framework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
-        require_once "vendor".DIRECTORY_SEPARATOR."samoscon".DIRECTORY_SEPARATOR."controller-framework".DIRECTORY_SEPARATOR."{$class_name}.php";
-    }
     if(file_exists("MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
         require_once "MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
     }
