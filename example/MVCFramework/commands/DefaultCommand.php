4d72cf2409168085f33e55cc5c9d829f0cc61b44
samoscon <53895420+samoscon@users.noreply.github.com>
2025-11-17 19:13:49 +0100

    Update DefaultCommand.php

--- example/MVCFramework/commands/DefaultCommand.php
+++ example/MVCFramework/commands/DefaultCommand.php
@@ -1,73 +1,53 @@
 <?php
 /**
- * DefaultCommand.php
+ * Specialization of a Command
  *
- * @package commands
- * @version 4.0
- * @copyright (c) 2024, Dirk Van Meirvenne
+ * @package commands\admin
+ * @version 1.0
+ * @copyright (c) 2025, Dirk Van Meirvenne
  * @author Dirk Van Meirvenne <van.meirvenne.dirk at gmail.com>
  */
-namespace commands;
+namespace commands\admin;
 
 /**
- * Subclass of Command. Must be at minimum present in the MVC framework. 
- * Will lead the visitor of your website to the first page (home screen, welcome screen, login screen, etc.)
+ * Specialization of a Command
  *
- * @link ../graphs/controllers%20(Application%20Controller)%20Class%20Diagram.svg Controllers class diagram
  * @author Dirk Van Meirvenne <van.meirvenne.dirk at gmail.com>
  */
-class DefaultCommand extends \controllerframework\controllers\Command {
+class AdminHomeCommand extends \controllerframework\controllers\CommandDecorator {
     
     /**
-     * Concrete specialization of execute method in Command
+     * Specialization of the execute method of Command
      * 
-     * @param \registry\Request $request
-     * @return int Returns status of the executed command
+     * @param \controllerframework\registry\Request $request
+     * @return int or null
      */
     #[\Override]
-    public function doExecute(\controllerframework\registry\Request $request): int {
-        /** variables */
-        $passwordIsValid = $userNameIsFound = true;
-        $passwordIsEmpty = $userIsEmpty = false;
-
-        /** Check that the page was requested from itself via the POST method. */
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {    
-            //** request variables and validate them */
-            $user = filter_var($request->get("user"), FILTER_VALIDATE_EMAIL);
-            $userIsEmpty = $user ? false : true;
-
-            $password = filter_var($request->get("password"), FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $passwordIsEmpty = $password ? false : true;
-
-            $memberid = $this->reg->getLoginManager()->validateUsername($user);
-            if(! $memberid) {
-                $userNameIsFound = false; //Member is not found or is no longer active
-            } else {
-                $passwordIsValid = $this->reg->getLoginManager()->validatePassword($memberid, $password);
-            }
-            
-            $rememberMe = filter_var($request->get("rememberMe"), FILTER_DEFAULT) ? true : false;
-            
-            //** perform your post request via the controller */
-            if (!$userIsEmpty && $userNameIsFound && !$passwordIsEmpty && $passwordIsValid) {
-                $user = $this->reg->getLoginManager()->login($memberid, $rememberMe);
-                return $this->loginChecks($user);
-            } 
-        }
-        
-        $this->addResponses($request, ["passwordIsValid" => $passwordIsValid, "userNameIsFound" => $userNameIsFound, 
-            "passwordIsEmpty" => $passwordIsEmpty, "userIsEmpty" => $userIsEmpty]);
-        
-//        include (realpath(__DIR__ . "/../..") . "/Views/defaultView.php");
-        return self::CMD_DEFAULT;
+    public function doExecuteDecorator(\controllerframework\registry\Request $request): ?int {
+        /** Put your code here. Following lines are meant as an example */
+        $this->addResponses($request, [
+            'title' => _MAILFROMNAME.' Admin',
+            'labelActivitiesList' => 'Activiteiten', //Contains the title on top of the activitieslist
+            'labelMembersMenu' => 'Leden' //Contains the name of the search for members in the menu
+            ]); 
+            return null;
     }
-    
+
     /**
-     * Concrete specialization of setLoginLevel method in Command.
-     * Needs to set 1 of the concrete subclasses of \sessions\Login
+     * Specialization of initCommand
+     */
+    #[\Override]
+    public function initCommand(): void {
+        $classname = '\\'.(new \ReflectionClass(get_called_class()))->getName();
+        $classname = str_replace('commands', 'membersactivities\commands', $classname);
+        $this->setCommand(new $classname);        
+    }
+
+    /**
+     * Specialization of getLevelOfLoginRequired
      */
     #[\Override]
     protected function getLevelOfLoginRequired(): void {
-        $this->setLoginLevel(new \controllerframework\sessions\NoLoginRequired());
+        $this->setLoginLevel(new \controllerframework\sessions\AdminLogin());
     }
 }
