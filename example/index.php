47512534f1df73337b316cbdecb8f10b1ddabc66
Dirk Van Meirvenne <53895420+samoscon@users.noreply.github.com>
2026-09-09 17:02:26 +0200

    Update index.php

--- example/index.php
+++ example/index.php
@@ -1,14 +1,28 @@
-<?php 
-include './vendor/autoload.php';
+<?php
 
-//Upload classes within your project automatically
+include __DIR__ . '/vendor/autoload.php';
+
+// Upload classes within your project automatically
 spl_autoload_register(function ($class_name) {
-    if(preg_match('/\\\\/', $class_name)) {
+    if (preg_match('/\\\\/', $class_name)) {
         $class_name = str_replace('\\', DIRECTORY_SEPARATOR, $class_name);
     }
-    if(file_exists("MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php")) {
-        require_once "MVCFramework".DIRECTORY_SEPARATOR."{$class_name}.php";
+
+    $file = __DIR__ . DIRECTORY_SEPARATOR .
+            'MVCFramework' . DIRECTORY_SEPARATOR .
+            $class_name . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
     }
 });
 
-controllerframework\controllers\Controller::run();
+try {
+    controllerframework\controllers\Controller::run();
+} catch (\Throwable $e) {
+    http_response_code(500);
+
+    echo '<h1>Application initialization failed</h1>';
+    echo '<p>' . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . '</p>';
+    exit;
+}
